e85504d53c2f1876d03ec3359077dda85178367b
theblackarts <theblackartsproject@gmail.com>
2016-09-25 10:06:13 -0700

    Add deckManager to Game
    Build some basic logic for each turn.

--- TheBlackArts/src/core/Game.java
+++ TheBlackArts/src/core/Game.java
@@ -8,6 +8,9 @@ public class Game {
 	private int gameID;
 	private int totalTurns = 0; // Who's turn it is, is based on modulo 2 (0 is player 1, 1 is player 2)
 	
+	// Deck Manager
+	Deck deckManager = new Deck();
+	
 	// Turn Phases
 	private boolean refreshPhase;
 	private boolean drawPhase;
@@ -49,14 +52,29 @@ public class Game {
      */
     public void startGame(Player playerOne, Player playerTwo) {
     	System.out.println("Game ID: " + this.getGameID());
+    	
+    	// Get each Player's Deck
+    	ArrayList<Card> deckOne = playerOne.getDeck();
+    	ArrayList<Card> deckTwo = playerTwo.getDeck();
+    	
+    	// Shuffle each Player's Deck
+    	deckManager.shuffleDeck(deckOne);
+    	deckManager.shuffleDeck(deckTwo);
+    	
+    	// Deal Each Player's starting hand
+    	playerOne.setHand(deckManager.dealSevenCards(deckOne));
+    	playerTwo.setHand(deckManager.dealSevenCards(deckTwo));
+    	
     	// There needs to be some sort of loop that allows players to take turns until one of them goes to 0 HP
         while (!playerOneWin || !playerTwoWin) {
 
         	// Announce that it is player one (two)'s turn
         	if (totalTurns % 2 == 0) {
-        		System.out.println("It is " + playerOne.getFirstName() + "'s turn.");
+        		System.out.println("It is " + playerOne.getFirstName() + "'s turn."); // everything in here for Player
+        																			  // One's turn?
         	} else if (totalTurns % 2 == 1) {
-        		System.out.println("It is " + playerTwo.getFirstName() + "'s turn.");
+        		System.out.println("It is " + playerTwo.getFirstName() + "'s turn."); // and everything here for Player
+        		  																      // Two's turn?
         	} else {
         		System.out.println("playerTurn variable is broken; FIX!"); 
         	}
@@ -64,9 +82,45 @@ public class Game {
         	// Display the turn number (e.g. first turn is 1, second turn is 2, and so on)
         	System.out.println(":: Turn :: " + (totalTurns + 1));
 
-        	char decideYN;
+        	// Go through all the phases of a player's turn
+        	
+        	// ** (1) Refresh **
+        	refreshPhase = true;
+        	
+        	// For each Gold card, it should go from used to unused
+        	
+        	// For each Living Asset (Monster) it should go from attacked to not attacked
+        	
+        	// For each card that has a game mechanic that is triggered by Refresh, it should have it's behavior here
+        	
+        	refreshPhase = false;
+        	
+        	// ** (2) Draw **
+        	
+        	drawPhase = true;
+        	if (totalTurns != 0) {
+        		if (totalTurns % 2 == 0) {
+        			// deal from deck one to player one
+        		} else if (totalTurns % 2 == 1) {
+        			// deal from deck two to player two
+        		} else {
+        			System.out.println("Draw phase is broken. FIX!");
+        		}
+        		
+        	}
+        	
+        	drawPhase = false;
+        	
+        	// ** (3) Attack **
+        	
+        	// ** (4) Mine ** <-- start with this Phase and then work backwards
+        	
+        	// ** (5) Purchase **
+        	
+        	// ** (6) End **
         	
         	// Give player one the option to pass his or her turn
+        	char decideYN;
         	do {
         		System.out.print("Would you like to pass your turn? (Y/N):");
             	decideYN = input.next().charAt(0); // VALIDATE that this is working as intended, getting one char
