3b817e6b6e439a314f961bba13b79fde361d5167
The Black Arts Project <theblackartsproject@gmail.com>
2016-11-14 22:00:36 -0800

    Add Tolkein quote

--- TheBlackArts/src/core/Game.java
+++ TheBlackArts/src/core/Game.java
@@ -1,5 +1,15 @@
-/* GL & HF
- * Shuffle up and deal!
+/* 
+ * "All that is gold does not glitter,
+ * Not all those who wander are lost;
+ * The old that is strong does not wither,
+ * Deep roots are not reached by the frost.
+ * From the ashes a fire shall be woken,
+ * A light from the shadows shall spring;
+ * Renewed shall be blade that was broken,
+ * The crownless again shall be king."
+ *  --J. R. R. Tolkein
+ *
+ * GL & HF, Shuffle up and deal!
  */
 
 package core;
